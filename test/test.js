45e340abdc6f93c33050ce61b22f448c1b0239b2
Mathieu Lecarme <mlecarme@bearstech.com>
2013-01-07 16:58:16 +0100

    Fixtures.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,16 @@
 var should = require('should'),
+    fs = require('fs'),
     jsonstore = require('../lib/jsonstore');
 
 describe('Store', function() {
+    var PATH = './test/toto.json';
+
+    beforeEach(function(done){
+        fs.writeFile(PATH, JSON.stringify({'beuha': 'Aussi'}), function(err) {
+            if (err) throw err;
+            done();
+        })
+    });
     describe('read', function() {
         it('should read test file', function(done) {
             var s = new jsonstore.Store('./test/toto.json', function() {
