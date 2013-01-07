af35809b073fde52a1e204347113c82f7695ebff
Mathieu Lecarme <mlecarme@bearstech.com>
2013-01-07 17:32:24 +0100

    Handling file watch.

--- test/test.js
+++ test/test.js
@@ -5,11 +5,11 @@ var should = require('should'),
 describe('Store', function() {
     var PATH = './test/toto.json';
 
-    beforeEach(function(done){
+    beforeEach(function(done) {
         fs.writeFile(PATH, JSON.stringify({'beuha': 'Aussi'}), function(err) {
             if (err) throw err;
             done();
-        })
+        });
     });
     describe('read/write', function() {
         it('should read test file', function(done) {
@@ -25,6 +25,19 @@ describe('Store', function() {
                     JSON.parse(fs.readFileSync(PATH)).beuha.should.eql(42);
                     done();
                 });
+            });
+        });
+    });
+    describe('handling file watch', function() {
+        it('should watch file changed', function(done) {
+            var s = new jsonstore.Store(PATH, function() {
+                s.on('refresh', function() {
+                    s.data.beuha.should.eql('plop');
+                    done();
+                })
+                setTimeout(function() {
+                    fs.writeFileSync(PATH, JSON.stringify({'beuha': 'plop'}));
+                }, 1000); //Damned, fs mtime min diff is 1 second
             });
         });
     });
